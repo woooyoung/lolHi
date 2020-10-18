5ce228758024cfe7c16f0c77e017e7bbecea7a83
woooyoung <axdsw121@gmail.com>
2020-10-18 16:10:05 +0900

    20201018 Sign Up validity check

--- lolHi/src/main/java/com/sbs/example/lolHi/controller/usr/MemberController.java
+++ lolHi/src/main/java/com/sbs/example/lolHi/controller/usr/MemberController.java
@@ -2,12 +2,16 @@ package com.sbs.example.lolHi.controller.usr;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sbs.example.lolHi.dto.Member;
 import com.sbs.example.lolHi.service.MemberService;
 
 @Controller
@@ -27,5 +31,19 @@ public class MemberController {
 
 		return String.format("<script> alert('%d번 회원이 생성되였습니다.'); location.replace('/usr/article/list'); </script>",
 				id);
+	}
+
+	@RequestMapping("/usr/member/login")
+	public String showLogin() {
+		return "usr/member/login";
+	}
+
+	@RequestMapping("usr/member/doLogin")
+	@ResponseBody
+	public String doLogin(Model model, @RequestParam Map<String, Object> param, HttpSession session) {
+		String loginId = (String) param.get("loginId");
+		String loginPw = (String) param.get("loginPw");
+		Member member = memberService.getMemberByLoginId(loginId);
+		return "usr/member/login";
 	}
 }
--- lolHi/src/main/java/com/sbs/example/lolHi/service/MemberService.java
+++ lolHi/src/main/java/com/sbs/example/lolHi/service/MemberService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sbs.example.lolHi.dao.MemberDao;
+import com.sbs.example.lolHi.dto.Member;
 import com.sbs.example.lolHi.util.Util;
 
 @Service
@@ -19,6 +20,10 @@ public class MemberService {
 		int id = Util.getAsInt(param.get("id"));
 
 		return id;
+	}
+
+	public Member getMemberByLoginId(String loginId) {
+		return MemberDao.getMemberByLoginId(loginId);
 	}
 
 }
