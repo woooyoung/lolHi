85ec87626a9c935d3b2e29bf0facf1c68858fab5
woooyoung <axdsw121@gmail.com>
2020-10-25 15:28:20 +0900

    20201025 Before Action Interceptor

---
+++ lolHi/src/main/java/com/sbs/example/lolHi/interceptor/BeforeActionInterceptor.java
@@ -0,0 +1,40 @@
+package com.sbs.example.lolHi.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.sbs.example.lolHi.dto.Member;
+import com.sbs.example.lolHi.service.MemberService;
+
+@Component("beforeActionInterceptor") // 컴포넌트 이름 설정
+public class BeforeActionInterceptor implements HandlerInterceptor {
+	@Autowired
+	private MemberService memberService;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+		Member loginedMember = null;
+
+		if (session.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) session.getAttribute("loginedMemberId");
+			loginedMember = memberService.getMemberById(loginedMemberId);
+		}
+
+		request.setAttribute("isLogined", isLogined);
+		request.setAttribute("loginedMemberId", loginedMemberId);
+		request.setAttribute("loginedMember", loginedMember);
+
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+}
